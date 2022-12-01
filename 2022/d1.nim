6bf5d9a3b358c2a60e17b31f44ee50df60532330
Antoine Delègue <delegue.antoine@gmail.com>
2022-12-01 23:16:30 +0100

    Shorter solution (probably the smallest that remains readable)

--- 2022/d1.nim
+++ 2022/d1.nim
@@ -1,7 +1,6 @@
 import sequtils, sugar, strutils
 
-let lines = readFile("input1.txt").split("\n\n")
-    .map((x) => x.strip().splitLines().map((y) => parseInt(y)))
-
 proc sum(x: seq[int]): int = x.foldl(a + b, 0)
-echo max(lines.map((x) => sum(x)))
+echo readFile("input1.txt").split("\n\n")
+    .map((x) => x.strip().splitLines().map(parseInt))
+    .map(sum).max()
--- 2022/d1_2.nim
+++ 2022/d1_2.nim
@@ -1,8 +1,6 @@
 import sequtils, sugar, strutils, algorithm
 
 proc sum(x: seq[int]): int = x.foldl(a + b, 0)
-var lines = readFile("input1.txt").split("\n\n")
-    .map((x) => x.strip().splitLines().map((y) => parseInt(y))).map((x) => sum(x))
-lines.sort(cmp, Descending)
-
-echo sum(lines[0..2])
+echo readFile("input1.txt").split("\n\n")
+    .map((x) => x.strip().splitLines().map(parseInt)).map(sum)
+    .sorted(cmp, Descending)[0..2].sum()
