4ae3d534a456ceff3ae4bdeda2d77b53a3a51e69
Antoine Delègue <delegue.antoine@gmail.com>
2022-12-12 11:19:39 +0100

    Day 12 ! using astar library because don't wanna rewrite a*

---
+++ 2022/d12_2.nim
@@ -0,0 +1,63 @@
+import astar, strutils, sequtils, sugar, hashes
+
+let grid = readFile("input12.txt").strip().splitLines().map((x) => x.items.toSeq)
+var goal: (int,int)
+
+for y in 0..<grid.len:
+    for x in 0..<grid[y].len:
+        if grid[y][x] == 'E':
+            goal = (x,y)
+
+
+proc elevation( grid: seq[seq[char]], point: (int,int) ): int {.inline.} =
+    if point[1] >= 0 and point[1] < grid.len and
+        point[0] >= 0 and point[0] < grid[point[1]].len:
+        if grid[point[1]][point[0]] == 'E':
+            return 25
+        if grid[point[1]][point[0]] == 'S':
+            return 0
+        return grid[point[1]][point[0]].int - 'a'.int
+    else: return -1
+
+iterator neighbors*( grid: seq[seq[char]], point: (int,int)): (int,int) =
+    ## An iterator that yields the neighbors of a given point
+    let curr = elevation(grid, (point[0],point[1]))
+    var val = -1
+
+    val = elevation( grid, (point[0] + 1, point[1]) )
+    if val != -1 and val <= curr+1:
+        yield (point[0] + 1, point[1])
+
+    val = elevation( grid, (point[0] - 1, point[1]) )
+    if val != -1 and val <= curr+1:
+        yield (point[0] - 1, point[1])
+
+    val = elevation( grid, (point[0], point[1] + 1) )
+    if val != -1 and val <= curr+1:
+        yield (point[0], point[1] + 1)
+
+    val = elevation( grid, (point[0], point[1] - 1) )
+    if val != -1 and val <= curr+1:
+        yield (point[0], point[1] - 1)
+
+proc cost*(grid: seq[seq[char]], a, b: (int,int)): float = 1
+proc heuristic*( grid: seq[seq[char]], node, goal: (int,int)): float =
+    return (abs(node[0] - goal[0]) + abs(node[1] - goal[1])).float
+
+
+var bestStepCount = grid.len * grid[0].len
+
+for y in 0..<grid.len:
+    for x in 0..<grid[y].len:
+        if grid[y][x] == 'a':
+            var start = (x,y)
+
+            var stepCount = 0
+            for point in path[seq[seq[char]], (int,int), float](grid, start, goal):
+                inc stepCount
+            if stepCount == 0: continue
+            
+            if stepCount - 1 < bestStepCount:
+                bestStepCount = stepCount - 1 
+
+echo bestStepCount
